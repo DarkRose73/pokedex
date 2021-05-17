0da2b0f3718aa58c6d723c72700263a593f26306
Joan Salas <joan.salas.silva@gmail.com>
2021-05-17 17:31:34 -0400

    Agregar funcionalidad eliminar

--- js/index.js
+++ js/index.js
@@ -14,6 +14,26 @@ tinymce.init({
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 });
 
+const enviarProfeOak = async function () {
+  //Siempre this me devuelve una referencia al elemento que llamo a la funcion
+  //En este caso es el boton
+  let nro = this.nro;
+  let res = await Swal.fire({
+    title: "Desea realmente continuar?",
+    text: "Esta intentando enviar el pokemon al profesor Roble, esto no se puede revertir",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtontext: "Si! Hazlo"
+  });
+  if(res.isConfirmed){
+    pokemones.splice(nro, 1);
+    cargarTabla();
+    Swal.fire("Pokemon descartado", "Pokemon enviado al profesor Roble" , "info");
+  }else{
+    Swal.fire("Cancelado", "Operacion cancelada", "error");
+  }
+}
+
 const pokemones = []; //Definir arreglo/lista
 //Esto aplica para generar cualquier cosa dinamicamente
 const cargarTabla = () => {
@@ -37,26 +57,21 @@ const cargarTabla = () => {
     //Definir lo que va en la tabla
     tdNro.innerText = i + 1;
     tdNombre.innerText = p.nombre;
-    //TODO: Mostrar el icono por tipo
     let tipo = document.createElement("i");
     if (p.tipo == "1") {
       //Tipo planta
-      //<i class="fas fa-leaf"></i>
       tipo.classList.add("fas", "fa-leaf", "text-success", "fa-2x");
     }
     else if (p.tipo == "2") {
       //Tipo Fuego
-      //<i class="fas fa-fire"></i>
-      tipo.classList.add("fas", "fa-fire" ,"text-danger", "fa-2x");
+      tipo.classList.add("fas", "fa-fire", "text-danger", "fa-2x");
     }
     else if (p.tipo == "3") {
       //Tipo Electrico
-      //<i class="fas fa-bolt"></i>
       tipo.classList.add("fas", "fa-bolt", "text-warning", "fa-2x");
     }
     else if (p.tipo == "4") {
       //Tipo Agua
-      //<i class="fas fa-tint"></i>
       tipo.classList.add("fas", "fa-tint", "text-primary", "fa-2x")
     }
     else {
@@ -71,6 +86,15 @@ const cargarTabla = () => {
     //Cuando quiero definir directamente HTML, innerHTML
     tdDescripcion.innerHTML = p.descripcion;
     //TODO: Que hago con las acciones?
+
+    let boton = document.createElement("button");
+    boton.classList.add("btn", "btn-danger");
+    boton.innerText = "Enviar al profesor roble";
+    boton.nro = i;
+    tdAcciones.appendChild(boton);
+    tdAcciones.classList.add("text-center");
+
+    boton.addEventListener("click", enviarProfeOak);
 
     //5. Agregar los td al tr
     tr.appendChild(tdNro);
